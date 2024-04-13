c6e1e70509fa4b228a743a8a92a96c1e24f76bd9
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-13 22:16:06 +0545

    forward_prop

--- forward_prop.py
+++ forward_prop.py
@@ -23,3 +23,19 @@ class CreateCircle(Scene):
         self.add(input_layer_circles, input_layer_labels, input_title)
 
         # Create hidden layer circles
+        
+        hidden_layer_circles = VGroup(
+            Circle(radius=0.5, color=RED),
+            Circle(radius=0.5, color=RED),
+            Circle(radius=0.5, color=RED),
+        ).arrange(DOWN, buff=0.5).shift(2 * RIGHT)
+
+        # Create hidden layer labels
+        hidden_layer_labels = VGroup(
+            Text("2", font_size=20).move_to(
+                hidden_layer_circles[0].get_center()),
+            Text("3", font_size=20).move_to(
+                hidden_layer_circles[1].get_center()),
+            Text("4", font_size=20).move_to(
+                hidden_layer_circles[2].get_center())
+        )
