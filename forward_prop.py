0a0a818bb3a0e6031b525787c372743a80c9c7ef
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-13 22:14:15 +0545

    forward_prop

--- forward_prop.py
+++ forward_prop.py
@@ -7,3 +7,11 @@ class CreateCircle(Scene):
             Circle(radius=0.5, color=RED),
             Circle(radius=0.5, color=RED)
         ).arrange(DOWN, buff=0.5)
+        
+    # Create input layer labels
+        input_layer_labels = VGroup(
+            Text("2", font_size=20).move_to(
+                input_layer_circles[0].get_center()),
+            Text("3", font_size=20).move_to(
+                input_layer_circles[1].get_center())
+        )
