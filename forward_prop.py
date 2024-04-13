b6c75618b8b19de04522cffa2a8d04d62072daaa
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-13 22:13:47 +0545

    forward_prop

--- forward_prop.py
+++ forward_prop.py
@@ -1,1 +1,9 @@
 from manim import * 
+
+class CreateCircle(Scene):
+    def construct(self):
+        # Create input layer circles
+        input_layer_circles = VGroup(
+            Circle(radius=0.5, color=RED),
+            Circle(radius=0.5, color=RED)
+        ).arrange(DOWN, buff=0.5)
