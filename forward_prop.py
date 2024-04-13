ce5a77a1e89840b112379d0f740c43f18b023c44
Suman Paudel <dastonsuman1997@gmail.com>
2024-04-13 22:14:47 +0545

    forward_prop

--- forward_prop.py
+++ forward_prop.py
@@ -15,3 +15,11 @@ class CreateCircle(Scene):
             Text("3", font_size=20).move_to(
                 input_layer_circles[1].get_center())
         )
+        
+    # Create input layer title
+        input_title = Text("Input Layer", font_size=20).move_to(UP * 3)
+
+        # Add input layer elements to the scene
+        self.add(input_layer_circles, input_layer_labels, input_title)
+
+        # Create hidden layer circles
